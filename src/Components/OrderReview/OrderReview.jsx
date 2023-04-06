1c607a8a824cf24012bafde9d85c2bff37c1e8b6
fazlerabbyrifat <rifatfazlerabby@gmail.com>
2023-04-07 00:41:14 +0600

    initial data loaded

---
+++ src/Components/OrderReview/OrderReview.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OrderReview = () => {
+    return (
+        <div>
+            <h3>Place your order here</h3>
+        </div>
+    );
+};
+
+export default OrderReview;
